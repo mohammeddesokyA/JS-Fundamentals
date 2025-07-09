a70a810609c7943175aeb10b6696bf8e8ef05c0b
Mohammed Desoky <ms2425033@gmail.com>
2025-07-10 02:03:51 +0300

    Fix factorial recursion crash on NaN

--- 10-factorial.js
+++ 10-factorial.js
@@ -6,4 +6,9 @@ function factorial(n) {
 }
 
 const arg = parseInt(process.argv[2]);
-console.log(factorial(arg));
+
+if (isNaN(arg)) {
+  console.log(1);
+} else {
+  console.log(factorial(arg));
+}
